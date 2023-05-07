df522c1e84669fe844923462c21898c0739b0063
Mario Alberto Chávez <mario.chavez@gmail.com>
2023-05-07 11:39:52 -0600

    Implements Collection operations.

--- lib/chroma/resources/collection.rb
+++ lib/chroma/resources/collection.rb
@@ -13,7 +13,7 @@ module Chroma
         @metadata = metadata
       end
 
-      def self.create_collection(name, metadata = nil)
+      def self.create(name, metadata = nil)
         payload = {name:, metadata:, get_or_create: false}
 
         result = execute_request(:post, "#{Chroma.api_url}/collections", payload)
@@ -26,12 +26,42 @@ module Chroma
         end
       end
 
+      def self.get(name)
+        result = execute_request(:get, "#{Chroma.api_url}/collections/#{name}")
+
+        if result.success?
+          data = result.success.body
+          new(data["name"], data["metadata"])
+        else
+          raise_failure_error(result)
+        end
+      end
+
+      def self.list
+        result = execute_request(:get, "#{Chroma.api_url}/collections")
+
+        if result.success?
+          data = result.success.body
+          data.map { |item| new(item["name"], item["metadata"]) }
+        else
+          raise_failure_error(result)
+        end
+      end
+
+      def self.delete(name)
+        result = execute_request(:delete, "#{Chroma.api_url}/collections/#{name}")
+
+        return true if result.success?
+
+        raise_failure_error(result)
+      end
+
       def self.raise_failure_error(result)
         case result.failure.error
         in Exception => exception
           raise Chroma::APIConnectionError.new(exception.message)
         in Net::HTTPInternalServerError => response
-          if response.body.is_a?(String) && response.body.include?("ValueError")
+          if response.body.is_a?(String) && (response.body.include?("ValueError") || response.body.include?("IndexError"))
             raise Chroma::InvalidRequestError.new(result.failure.body, status: result.failure.status, body: result.failure.body)
           else
             raise Chroma::APIConnectionError.new(result.failure.body, status: result.failure.status, body: result.failure.body)
--- test/chroma/resources/test_collection.rb
+++ test/chroma/resources/test_collection.rb
@@ -31,42 +31,115 @@ class CollectionTest < Minitest::Test
       response_body: body
     )
 
-    collection = Chroma::Resources::Collection.create_collection("test-collection", {source: "test"})
+    collection = Chroma::Resources::Collection.create("test-collection", {source: "test"})
 
     assert_instance_of(Chroma::Resources::Collection, collection)
     assert_equal name, collection.name
     assert_equal deep_stringify_keys(metadata), collection.metadata
   end
 
-  def test_it_raises_api_connection_error_with_network_problems
+  def test_it_raises_api_connection_error_with_network_problems_create_collection
     stub_network_error("#{Chroma.api_url}/collections")
 
     assert_raises Chroma::APIConnectionError do
-      Chroma::Resources::Collection.create_collection("test-collection", {source: "test"})
+      Chroma::Resources::Collection.create("test-collection", {source: "test"})
     end
   end
 
-  def test_it_raises_invalid_request_error_with_invalid_parameters
+  def test_it_raises_invalid_request_error_with_invalid_parameters_create_collection
     stub_server_error("#{Chroma.api_url}/collections", %({"error":"ValueError('Expected collection name that (1) contains 3-63 characters, (2) starts and ends with an alphanumeric character, (3) otherwise contains only alphanumeric characters, underscores or hyphens (-), (4) contains no two consecutive periods (..) and (5) is not a valid IPv4 address, got ruby-index-4 invalid')"}))
 
     assert_raises Chroma::InvalidRequestError do
-      Chroma::Resources::Collection.create_collection("test-collection", {source: "test"})
+      Chroma::Resources::Collection.create("test-collection", {source: "test"})
     end
   end
 
-  def test_it_raises_api_connection_error_with_server_problems
+  def test_it_raises_api_connection_error_with_server_problems_create_collection
     stub_server_error("#{Chroma.api_url}/collections")
 
     assert_raises Chroma::APIConnectionError do
-      Chroma::Resources::Collection.create_collection("test-collection", {source: "test"})
+      Chroma::Resources::Collection.create("test-collection", {source: "test"})
     end
   end
 
-  def test_it_raises_api_error
+  def test_it_raises_api_error_create_collection
     stub_client_error("#{Chroma.api_url}/collections")
 
     assert_raises Chroma::APIError do
-      Chroma::Resources::Collection.create_collection("test-collection", {source: "test"})
+      Chroma::Resources::Collection.create("test-collection", {source: "test"})
+    end
+  end
+
+  def test_it_gets_a_collection
+    body = request_body
+    name = body.fetch(:name)
+    metadata = body.fetch(:metadata)
+
+    stub_collection_request(
+      "#{Chroma.api_url}/collections/#{name}",
+      method: :get,
+      request_body: "",
+      response_body: body
+    )
+
+    collection = Chroma::Resources::Collection.get("test-collection")
+
+    assert_instance_of(Chroma::Resources::Collection, collection)
+    assert_equal name, collection.name
+    assert_equal deep_stringify_keys(metadata), collection.metadata
+  end
+
+  def test_it_raises_invalid_request_error_with_invalid_name_get_collection
+    body = request_body
+    name = body.fetch(:name)
+
+    stub_server_error("#{Chroma.api_url}/collections/#{name}", %({"error"=>"ValueError('Collection #{name} does not exist')"}))
+
+    assert_raises Chroma::InvalidRequestError do
+      Chroma::Resources::Collection.get("test-collection")
+    end
+  end
+
+  def test_it_gets_a_collection_list
+    body = request_body
+
+    stub_collection_request(
+      "#{Chroma.api_url}/collections",
+      method: :get,
+      request_body: "",
+      response_body: [body]
+    )
+
+    collections = Chroma::Resources::Collection.list
+
+    assert_instance_of(Chroma::Resources::Collection, collections.first)
+    assert_equal 1, collections.size
+  end
+
+  def test_it_deletes_collection
+    body = request_body
+    name = body.fetch(:name)
+
+    stub_collection_request(
+      "#{Chroma.api_url}/collections/#{name}",
+      method: :delete,
+      request_body: "",
+      response_body: [body]
+    )
+
+    deleted = Chroma::Resources::Collection.delete("test-collection")
+
+    assert deleted
+  end
+
+  def test_it_raises_invalid_request_error_deleting_not_existing_collection
+    body = request_body
+    name = body.fetch(:name)
+
+    stub_server_error("#{Chroma.api_url}/collections/#{name}", %({"error"=>"IndexError('list index out of range')"}))
+
+    assert_raises Chroma::InvalidRequestError do
+      Chroma::Resources::Collection.delete("test-collection")
     end
   end
 
@@ -74,7 +147,7 @@ class CollectionTest < Minitest::Test
 
   def stub_collection_request(url, method: :get, status: 200, request_body: {}, response_body: {})
     @stubs << stub_request(method, url)
-      .with(body: request_body.to_json)
+      .with(body: request_body.is_a?(Hash) ? request_body.to_json : request_body)
       .to_return(status:, body: response_body.to_json, headers: {"Content-Type": "application/json"})
   end
 
