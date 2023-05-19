8d3763a7d41e300efa968f7a4312ae89b33a1b0c
Mario Alberto Chávez <mario.chavez@gmail.com>
2023-05-19 12:08:05 -0600

    Configure test_helper to use Ruby Next

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+ENV["RUBY_NEXT_TRANSPILE_MODE"] = "rewrite"
+ENV["RUBY_NEXT_EDGE"] = "1"
+ENV["RUBY_NEXT_PROPOSED"] = "1"
+require "ruby-next/language/runtime" unless ENV["CI"]
+
 require "chroma-db"
 
 require "minitest/autorun"
