2ec72b74404ddfdc71c8ced26decffbb3f2dd1ef
Gabriel Figueira <figueira.gabriel11@gmail.com>
2024-05-28 14:32:50 -0300

    feat: Add token on header if have the envieroment

--- lib/chroma/api_operations/request.rb
+++ lib/chroma/api_operations/request.rb
@@ -118,8 +118,7 @@ module Chroma
           request.content_type = "application/json"
           request.body = params.to_json if params.size > 0
           request.basic_auth(uri.user, uri.password) if !uri.user.nil?
-
-          request
+          request['X-Chroma-Token'] = ENV.fetch('CHROMA_SERVER_AUTHN_CREDENTIALS', nil) if ENV.fetch('CHROMA_SERVER_AUTHN_CREDENTIALS', nil)
         end
       end
 
